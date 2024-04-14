3312d21ab2203b57cac643f5f75c9195d03033e1
Nhaniso Undenge <nhanitech@gmail.com>
2024-04-14 02:36:29 +0200

    feat: Filtering and item search test

---
+++ resources/js/components/Pagination.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in paginatedItems" :key="item.id">{{ item.name }}</li>
+        </ul>
+        <button @click="prevPage">Previous</button>
+        <button @click="nextPage">Next</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentPage: 1,
+            itemsPerPage: 5,
+            items: [
+                { id: 1, name: 'Item 1' },
+                { id: 2, name: 'Item 2' },
+                // More items...
+            ],
+        };
+    },
+    computed: {
+        paginatedItems() {
+            const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+            return this.items.slice(startIndex, startIndex + this.itemsPerPage);
+        },
+    },
+    methods: {
+        prevPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+            }
+        },
+        nextPage() {
+            const totalPages = Math.ceil(this.items.length / this.itemsPerPage);
+            if (this.currentPage < totalPages) {
+                this.currentPage++;
+            }
+        },
+    },
+};
+</script>
---
+++ resources/js/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input type="text" v-model="searchQuery" placeholder="Search...">
+        <ul>
+            <li v-for="item in filteredItems" :key="item.id">{{ item.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchQuery: '',
+            items: [
+                { id: 1, name: 'Item 1' },
+                { id: 2, name: 'Item 2' },
+                { id: 3, name: 'Another Item' },
+            ],
+        };
+    },
+    computed: {
+        filteredItems() {
+            return this.items.filter(item =>
+                item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+            );
+        },
+    },
+};
+</script>
